dac5a6ccbe83386a4b0cff8bfb40685f7680429f
Fabio mendes ribeiro <servermr@hotmail.com>
2018-07-07 20:40:41 -0300

    Formulario de cadastro funcionando...

--- ajax/languages/pt-br/register.lang.php
+++ ajax/languages/pt-br/register.lang.php
@@ -1,36 +1,18 @@
 <?php
-	 /* 	$AccRegister01 = "Você deixou campos obrigatórios em branco. Por favor, confira.";
-		
-		$AccRegister02 = "As senhas que você usou são diferentes! Por favor digite as duas senhas IGUAIS!";
-		
-		$AccRegister03 = "Código digitado incorretamente!";
-		
-		$AccRegister04 = "Não use caracteres especiais em seu nome de usuário. Apenas letras (A-Z , a-z) e/ou números são permitidos.";
-		
-		$AccRegister05 = "São necessários 7 NÚMEROS para a <strong>chave de segurança</strong>";
-		
-		$AccRegister06 = "O e-mail ([mail_addr]) já foi usado em nosso banco de dados. Por favor use outro endereço de e-mail!";
-		
-		$AccRegister07 = "O nome de usuário (".$memb___id.") já está sendo usado! Por favor, escolha outro nome de usuário.";
-		
-		$AccRegister08 = "Não foi possível criar seu cadastro neste momento. Por favor tente mais tarde ou informe o suporte.";
-		
-		$AccRegister09 = "Olá, [memb_name]!<br />Sua conta foi criada com sucesso!<br />Obrigado por se juntar a nós!<br />Esperamos que você tenha momentos felizes e descontraídos jogando em nossos servidores.<br />Estamos enviando um e-mail neste exato momento explicando como validar seu endereço e-mail.<br />Quaisquer dúvidas poderão ser tiradas em nosso sistema de suporte no próprio site.<br /><br />Divirta-se!<br /><br /><br />";
-		
-		$AccRegister10 = "Os e-mails digitados não conferem! Por favor digite os dois e-mails IGUAIS!";
-		
-		$AccRegister11 = "O e-mail digitado parece inválido. Verifique se digitou corretamente.";
-		
-		$AccRegister12 = "O nome de usuário deve ter entre 4 e 10 letras e/ou números.";
-		
-		$AccRegister13 = "A senha deve ter entre 4 e 10 letras e/ou números.";
-		
-		$AccRegister14 = "Não use caracteres especiais em sua senha. Apenas letras (A-Z , a-z) e/ou números são permitidos.";
-		
-		
-		$AccRegister15 = "Cadastro no MuRuby"; //Mail Confirmation Subject
-		
-		//Mail Confirmation Content (html enabled)
-		$AccRegister16 = "<p>Ol&aacute;, [memb_name]!</p><p>Você registrou-se recentemente no MuRuby.</p><p>Para ativar sua conta, é necessário que você abra o link abaixo em seu navegador:<br />http://www.muruby.net/?c=MailActivate/[verify_hash]/[memb___id]<br /><a href='http://www.muruby.net/?c=MailActivate/[verify_hash]/[memb___id]'>ATIVAR CONTA</a></p><p>Lembrando seus dados:<br />Nome de Usuário: [memb___id]<br />Senha: (oculta) <br />E-mail: [mail_addr] <br />Pergunta Secreta: [fpas_ques] <br />Resposta Secreta: [fpas_answ] <br /><b>NÃO ESQUEÇA SEUS DADOS, PRINCIPALMENTE SUA SENHA!<br />É A ÚNICA FORMA DE MANTER SUA CONTA SEMPRE ATIVA E SEGURA!</b><br /><br />
-		<p>Agradecemos a preferência,</p><p>Equipe MuRuby</p><p>* Esta mensagem foi gerada automaticamente em nossos servidores, por favor não responda.</p>";
-*/
+	
+DEFINE("REGISTER_MSG_00", "Desculpe não foi possivel criar sua conta.");
+DEFINE("REGISTER_MSG_01", "É obrigatorio validar o reCAPTCHA");
+DEFINE("REGISTER_MSG_02", "Todos os campos devem ser preenchidos");
+DEFINE("REGISTER_MSG_03", "As senhas digitadas não conferem");
+DEFINE("REGISTER_MSG_04", "Sua senha deve ter entre 4 e 10 caracteres");
+DEFINE("REGISTER_MSG_05", "Sua senha só pode conter letras e numeros");
+DEFINE("REGISTER_MSG_06", "Os emails digitados não conferem");
+DEFINE("REGISTER_MSG_07", "Por favor digite um email valido.");
+DEFINE("REGISTER_MSG_08", "Seu login só pode conter letras e numeros");
+DEFINE("REGISTER_MSG_09", "Seu login deve ter entre 4 e 10 digitos");
+DEFINE("REGISTER_MSG_10", "Esse email ja se encontra cadastrado em nosso sistema.<br> se você ja possui uma conta <a href='/login'> click aqui </a> para efetuar o login.");
+DEFINE("REGISTER_MSG_11", "Esse login ja se encontra cadastrado em nosso sistema.<br> se você ja possui uma conta <a href='/login'> click aqui </a> para efetuar o login.");
+
+
+DEFINE("REGISTER_MSG_ERROR", "Por favor atualize a pagina e tente novamente!!!");
+DEFINE("REGISTER_MSG_999", "Cadastro efetuado com sucesso!!!");
--- ajax/modules/classes/register.class.php
+++ ajax/modules/classes/register.class.php
@@ -20,9 +20,49 @@ if ( class_exists( "Register" ) == false ) {
 		$type = 0;
 		switch($this->validateInf())
 		{
-			default:
-				$msg = "CONTA CRIADA COM SUCESSO!!!";
+			case 0:
+				$msg = REGISTER_MSG_00;
 				break;
+			case 1:
+				$msg = REGISTER_MSG_01;
+				break;
+			case 2:
+				$msg = REGISTER_MSG_02;
+				break;
+			case 3:
+				$msg = REGISTER_MSG_03;
+				break;
+			case 4:
+				$msg = REGISTER_MSG_04;
+				break;
+			case 5:
+				$msg = REGISTER_MSG_05;
+				break;
+			case 6:
+				$msg = REGISTER_MSG_06;
+				break;
+			case 7:
+				$msg = REGISTER_MSG_07;
+				break;
+			case 8:
+				$msg = REGISTER_MSG_08;
+				break;
+			case 9:
+				$msg = REGISTER_MSG_09;
+				break;
+			case 10:
+				$msg = REGISTER_MSG_10;
+				break;
+			case 11:
+				$msg = REGISTER_MSG_11;
+				break;
+			case 999:
+				$msg = REGISTER_MSG_999;
+				break;
+				
+		default:
+		$msg = REGISTER_MSG_ERROR;
+		break;
 				
 		}
 			
@@ -34,23 +74,35 @@ if ( class_exists( "Register" ) == false ) {
 		private function validateInf(){
 		foreach ($_POST as $key => $value){$$key = $value;} 
 		$recaptcha = $_POST['g-recaptcha-response'];
-		if(!$this->reCAPTCHA($recaptcha)) {return 1;} // não validou o captcha
+	//	if(!$this->reCAPTCHA($recaptcha)) {return 1;} // não validou o captcha
 		//usuario, email ou senha em branco
-		if(empty($reg_login) || empty($memb__pwd) || empty($mail_addr)){return 2;}
+		if(empty($reg_login) || empty($reg_password) || empty($reg_email)){return 2;}
 		// as senhas não confere
-		if ($memb__pwd != $memb__pwd2){return 3;}
+		if ($reg_password != $reg_repassword){return 3;}
 		//Tamanho da senha e diferente do permitido
-		if(strlen($memb__pwd) < 4 || strlen($memb__pwd) > 10){ return 4;}
+		if(strlen($reg_password) < 4 || strlen($reg_password) > 10){ return 4;}
 		// senha so pode conter letras e numeros
-		if(!ctype_alnum($memb__pwd)){ return 5;}
+		if(!ctype_alnum($reg_password)){ return 5;}
 		//verifica se o email informado e o re email conferem 
-		if ($mail_addr != $mail_addr2){ return 6;}
+		if ($reg_email != $reg_reemail){ return 6;}
 		// verifica se foi digitado um email valido 
-		if(!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $mail_addr)){ return 7;}
+		if(!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $reg_email)){ return 7;}
 		//Login pode conter somente letras e numeros
 		if(!ctype_alnum($reg_login)){return 8;}
 		// verifica o tamanho do login
 		if(strlen($reg_login) < 4 || strlen($reg_login) > 10){ return 9;}
+		//verifica se o email ja esta cadastrado
+		if($this->checkemail($reg_email)){ return 10;}
+		// verifica se a conta ja esta cadastrada
+		if($this->checkLogin($reg_login)){ return 11;}
+		
+		$sno__numb = "1234567";	
+			
+		$result = $this->registerAccount($reg_login,$reg_password,$reg_nick,$sno__numb,$reg_email,NULL,NULL);	
+		if($result){ return 999;}
+			else{
+				return 0;
+			}
 			
 		}
 		
@@ -68,35 +120,50 @@ if ( class_exists( "Register" ) == false ) {
 		}
 		
 		// verifica se o email ja não esta registrado no banco de dados
-        private function checkemail(){
+        private function checkemail($mail_addr){
+		$sql = "SELECT mail_addr FROM MEMB_INFO WHERE mail_addr = '$mail_addr'";
+		$result = $this->selectDB($sql);
+			if(count($result) > 0) return true;
+		return false;
+		}
+
+		// verifica se o email ja não esta registrado no banco de dados
+        private function checkLogin($reg_login){
 		$sql = "SELECT memb___id FROM MEMB_INFO WHERE memb___id = '$reg_login'";
 		$result = $this->selectDB($sql);
-		return count($result);
+					if(count($result) > 0) return true;
+		return false;
 		}
 
 		
 		//Cria a noca conta
-		private function registerAccount()
+		private function registerAccount($reg_login,$reg_password,$memb_name,$sno__numb,$reg_email,$fpas_ques,$fpas_answ)
 		{
-			
-			$sql ="INSERT INTO ". DATABASE .".dbo.MEMB_INFO (memb___id,memb__pwd,memb_name,sno__numb,post_code,addr_info,addr_deta,tel__numb,mail_addr,phon_numb,fpas_ques,fpas_answ,job__code, appl_days,modi_days,out__days,true_days,mail_chek,bloc_code,ctl1_code)
+			/*           ,[bloc_code]
+           ,[ctl1_code]
+           ,[Coin1]
+           ,[Coin2]
+           ,[Coin3]
+           ,[AccountLevel]
+           ,[AccountDays]*/
+			$sql ="INSERT INTO ". DATABASE .".dbo.MEMB_INFO (memb___id,memb__pwd,memb_name,sno__numb,post_code,addr_info,addr_deta,tel__numb,mail_addr,phon_numb,
+			fpas_ques,fpas_answ,job__code, appl_days,modi_days,out__days,true_days,mail_chek,bloc_code,ctl1_code)
 			VALUES 
-('$reg_login','$memb__pwd','$memb_name','$sno__numb','s-n','11111','','0','$mail_addr','','$fpas_ques','$fpas_answ','1','2003-11-23','2003-11-23','2003-11-23','2003-11-23','1','0','1')";
+('$reg_login','$reg_password','$memb_name','$sno__numb','s-n','11111','','0','$reg_email','','$fpas_ques','$fpas_answ','1','2003-11-23','2003-11-23','2003-11-23','2003-11-23','1','0','1')";
 
+			
 			$result = $this->insertDB($sql);
 
-			if($result > 0){
+			if(count($result) > 0) return true;
+			return false;
+				/*
 			$date = date('M d Y h:i');
 			$date = date('M d Y h:i', strtotime("+7 days",strtotime($date)));			
 $sql ='UPDATE '. DATABASE .'.[dbo].[MEMB_INFO] SET [AccountLevel] = \'1\',[AccountExpireDate] = \''.$date.'\'
  WHERE [memb___id] = \''.$reg_login.'\'';
 			$result = $this->updateDB($sql);	
-				
-			$type = 0;
-			$return .="Cadastro efetuado com sucesso";
-			}else{
-			$return .="Desculpe nao foi possivel efetuar o cadastro no momento.";
-			}
+				*/
+
 			
 		
 		}
