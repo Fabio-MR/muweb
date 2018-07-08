8dffbace5e6c0021addbdec6f9aeefc8516cf50c
Fabio mendes ribeiro <servermr@hotmail.com>
2018-07-07 22:15:42 -0300

    add usercp

---
+++ templates/blue/usercp.tpl.php
@@ -0,0 +1,228 @@
+{#INCLUDE:header}
+	<body>
+		{#INCLUDE:header[LOGOUT]}
+		<div id="container">
+
+			<div class="wrapper">
+				{#INCLUDE:sidebar}
+				<div id="content">
+
+					<div class="article">
+						<!--
+                        <div id="warn-info">With our unique <a href="https://norebirth.com/topic/927-rampage-x20-progressive-content/">progressive system</a> it's never too late to join the Rampage server!</div>
+-->
+						<h2><i class="icons icon-game"></i>Game services</h2>
+						<div class="spacer3"></div>
+
+<div class="main">
+   <div id="control-menu" class="frame">
+      <a href="account" class="active"><i id="ico-account"></i>Services</a>
+      <a href="vip"><i id="ico-vip"></i>VIP</a>
+      <a href="market"><i id="ico-market"></i>Market</a>
+      <a href="auction"><i id="ico-auction"></i>Auction</a>
+      <a href="achievement"><i id="ico-achievement"></i>Achievements</a>
+      <a href="castle"><i id="ico-castle"></i>Castle</a>
+      <a href="referral"><i id="ico-referral"></i>Referral</a>
+     </div>
+  </div>
+<div class="spacer2"></div><div class="main">
+ 
+  
+    
+    <div id="board">
+          <div data-id="1" class="" style="display: none;">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 July 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/9378-Hashi/" target="_blank">Hashi</a> <span>(Rampage, x20)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="2" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 July 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/9293-bob1/" target="_blank">bob1</a> <span>(Rampage, x20)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="3" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 July 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/9230-Fury/" target="_blank">Fury</a> <span>(Rampage, x20)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="4" style="display: block;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 July 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/2586-buriza/" target="_blank">buriza</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="5" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 July 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/6699-ruje1/" target="_blank">ruje1</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="6" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 June 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/3273-Wnuq/" target="_blank">Wnuq</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="7" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 June 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/7255-stella/" target="_blank">stella</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="8" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 June 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/6716-GuessWho/" target="_blank">GuessWho</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="9" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 May 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/7186-LEGO/" target="_blank">LEGO</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="10" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 May 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/6483-warmize/" target="_blank">warmize</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="11" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 May 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/7114-Huizai/" target="_blank">Huizai</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="12" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 April 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/1543-MaxOn/" target="_blank">MaxOn</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+          <div data-id="13" style="display: none;" class="">
+        
+        <div class="descr">
+        <div class="time small help_text" title="01 April 2018, 00:00">00:00</div>
+                  <h3><a href="#?/profile/7242-bingo/" target="_blank">bingo</a> <span>(Miracle, x50)</span></h3>
+                  <span>Has won Gold VIP status renewal for 1 month!</span>
+        </div>
+      </div>
+        </div>
+    <div class="spacer2"></div>
+    
+    <script type="text/javascript">
+      $(function() {
+        $("#board").changeWords({
+          time: 5000,
+          selector: "div"
+        });
+      });
+    </script>
+    
+  <div class="tabs">
+    <ul>
+          <li class="active" data-page="0">Character</li>
+          <li data-page="1">Currency</li>
+          <li data-page="2">Extra</li>
+        <div id="tabs-right"><a class="mirror non" href="bonus">Gifts from administrators</a></div>
+    </ul>
+    
+    <div>
+          <div style="display: block;">
+      
+         
+          <a class="nice-button" href="account/teleport/#top" id="teleport">Teleport</a>
+         
+          <a class="nice-button" href="account/change-name/#top" id="change-name">Change name</a>
+         
+          <a class="nice-button" href="account/stats/#top" id="stats">Add stats</a>
+         
+          <a class="nice-button" href="account/stats-reset/#top" id="stats-reset">Reset stats</a>
+                
+        <div class="spacer3"></div>
+        
+         
+        
+                
+                
+      </div>
+          <div style="display: none;">
+      
+         
+          <a class="nice-button" href="account/exchange/#top" id="exchange">W Coin transfer</a>
+         
+          <a class="nice-button show-popup" href="account/transfer/#top" id="transfer">Zen transfer</a>
+                
+        <div class="spacer3"></div>
+        
+         
+        
+                
+                
+      </div>
+          <div style="display: none;">
+      
+         
+          <a class="nice-button" href="account/data-hide/#top" id="data-hide">Invisibility</a>
+         
+          <a class="nice-button" href="account/duplicate/#top" id="duplicate">Delete duplicated items</a>
+         
+          <a class="nice-button" href="account/cleanitem/#top" id="cleanitem">Clear your inventory</a>
+                
+        <div class="spacer3"></div>
+        
+         
+        
+                
+                
+      </div>
+        </div> 
+  </div>
+
+  <div class="clear"></div>
+
+  </div>
+
+
+
+
+
+
+
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+
+		</div>
+
+		{#INCLUDE:footer}
+	</body>
+	</html>
